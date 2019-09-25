de28467d38d76962710bd08a7eea7bb67b3842f1
dhruvaraju <49dhruva@gmail.com>
2019-09-25 22:45:11 +0530

    Creating an inital app for logging text provided

---
+++ initialApp.js
@@ -0,0 +1,14 @@
+//Initial App
+//Function to console log given data 
+function sayhello(name){
+    console.log('Hello '+ name);
+}
+
+//Invoking function sending an input
+sayhello('MindHunter');
+
+//Window object is not available in node js if you log it it will through an error
+//console.log(window);
+
+//Global is the root object in node js
+//console.log(global);
